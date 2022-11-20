e54a7573b5c1dbc8455af3e5f26a4b9fb625d164
Stereo2025 <teneloks@gmail.com>
2022-11-20 20:08:33 +0300

    ver 2

--- global_variables.py
+++ global_variables.py
@@ -6,3 +6,6 @@ COMMENTS_PATH = BASE_DIR.joinpath('json_lists', 'comments.json')
 BOOKMARKS_PATH = BASE_DIR.joinpath('json_lists', 'bookmarks.json')
 PATH_LOGS_CONFIG = BASE_DIR.joinpath('logs', 'config.log')
 PATH_MAIN_LOGS = BASE_DIR.joinpath('logs', 'main.log')
+
+
+# dckr_pat_vaDNuJu2f6T6Rk34YLkjEDlGOR4 - docker-key
